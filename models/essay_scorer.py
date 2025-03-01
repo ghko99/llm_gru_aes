cca44d418efa82541d2b4bafdf3c35db5494d577
ghko99 <ghko99@gmail.com>
2025-03-02 02:09:31 +0900

    modify essay scorer

--- models/essay_scorer.py
+++ models/essay_scorer.py
@@ -5,19 +5,19 @@ import torch
 
 class EssayScorer(nn.Module):
     def __init__(self, output_dim=11, hidden_dim=128):
+        
         super(EssayScorer, self).__init__()
         self.bert = get_kobert_model()
         self.gru = GRUScoreModule(output_dim=output_dim, hidden_dim=hidden_dim)
 
-    def forward(self, batch, labels=None):
-        
-        input_ids = batch['input_ids']
-        attention_mask = batch['attention_mask']
+    def forward(self, input_ids, attention_mask, labels=None):
+
         B, S, L = input_ids.size()
         flattened_input_ids = input_ids.view(B * S, L)
         flattened_attention_mask = attention_mask.view(B * S, L)
-
+        
         outputs = self.bert(input_ids=flattened_input_ids, attention_mask=flattened_attention_mask)
+        
         embedded_outputs = outputs[0][:,0,:]
 
         embedded_outputs = embedded_outputs.view(B,S,-1)

dda3ba92f885f5d516c2b39d5faaac663c7f3560
ghko99 <ghko99@gmail.com>
2025-03-02 02:07:25 +0900

    essay dataset

--- data_provider/essay_dataset.py
+++ data_provider/essay_dataset.py
@@ -8,18 +8,17 @@ class EssayDataset(Dataset):
         self.scores = torch.tensor(scores, dtype=torch.float32)
         self.tokenizer = tokenizer
         self.max_length = max_length
+        self.inputs =  [self.tokenizer.batch_encode_plus(text, max_length = self.max_length, padding="max_length", truncation=True) for text in tqdm(texts)]
+        self.input_ids = [torch.tensor(input['input_ids']) for input in tqdm(self.inputs)]
+        self.attention_mask = [torch.tensor(input['attention_mask']) for input in tqdm(self.inputs)]
+
     def __len__(self):
         return len(self.texts)
 
     def __getitem__(self, idx):
 
-        inputs = self.tokenizer.batch_encode_plus(self.texts[idx], 
-                                                  max_length=self.max_length,
-                                                  padding="max_length",
-                                                  truncation=True
-                                                  )
-        input_ids = torch.tensor(inputs['input_ids']).cuda()
-        attention_mask = torch.tensor(inputs['attention_mask']).cuda()
+        input_ids = self.input_ids[idx]
+        attention_mask = self.attention_mask[idx]
         return {
             'input_ids': input_ids,
             'attention_mask': attention_mask,
